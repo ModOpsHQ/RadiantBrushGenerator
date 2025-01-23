1cb3126317938ea614d55f6984d783072868a19c
Phil81334 <modopshq@outlook.com>
2025-01-23 18:34:43 +0000

    u

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main():
             "atoll_ship_metal_rust1",       # Right (visible in side view)
             "berlin_ceiling_concrete_tile", # Front (visible in front view, near)
             "atoll_ship_metal_ribs_blend"   # Left (visible in side view)
-        ], position=(256,0,0), rotation=(0,0,0)),
+        ], position=(256,0,0), rotation=(90,0,0)),
         create_brush(32,64,8, [
             "caulk",                        # Bottom (visible in top view)
             "berlin_roof_wood_dirty",       # Top (visible in top view)
@@ -33,7 +33,7 @@ def main():
             "atoll_ship_metal_rust1",       # Right (visible in side view)
             "berlin_ceiling_concrete_tile", # Front (visible in front view, near)
             "atoll_ship_metal_ribs_blend"   # Left (visible in side view)
-        ], position=(0,256,0), rotation=(0,0,0)),
+        ], position=(0,256,0), rotation=(0,90,0)),
         create_brush(32,64,8, [
             "caulk",                        # Bottom (visible in top view)
             "berlin_roof_wood_dirty",       # Top (visible in top view)
@@ -41,7 +41,7 @@ def main():
             "atoll_ship_metal_rust1",       # Right (visible in side view)
             "berlin_ceiling_concrete_tile", # Front (visible in front view, near)
             "atoll_ship_metal_ribs_blend"   # Left (visible in side view)
-        ], position=(0,0,256), rotation=(0,0,0))
+        ], position=(0,0,256), rotation=(0,0,90))
 
     ], path)
 
